30668ad5a8817dd98e3b2ee515da331be0565cf7
gabriel saito <gabrielpereira050201@hotmail.com>
2025-07-13 15:26:43 -0300

    chore: remove Playwright workflow and update command in Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,7 +70,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'yarn dev',
+    command: 'npm run dev',
     url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
   },
